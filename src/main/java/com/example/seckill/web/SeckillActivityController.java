f29da9b2600ffcadbc12fa7d9c9a116d3cadaa26
wusibing <1915563868@qq.com>
2024-06-19 23:19:47 +0800

    Implement add seckill activity

---
+++ src/main/java/com/example/seckill/web/SeckillActivityController.java
@@ -0,0 +1,63 @@
+package com.example.seckill.web;
+
+import com.example.seckill.db.dao.SeckillActivityDao;
+import com.example.seckill.db.po.SeckillActivity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+@Controller
+public class SeckillActivityController {
+
+    @Autowired
+    private SeckillActivityDao seckillActivityDao;
+
+    // 发布页面
+    @RequestMapping("/addSeckillActivity")
+    public String addSeckillActivity() {
+        return "add_activity";
+    }
+
+    // 提交发布
+//    @ResponseBody
+    @RequestMapping("/addSeckillActivityAction")
+    public String addSeckillActivityAction(
+            @RequestParam("name") String name,
+            @RequestParam("commodityId") long commodityId,
+            @RequestParam("seckillPrice") BigDecimal seckillPrice,
+            @RequestParam("oldPrice") BigDecimal oldPrice,
+            @RequestParam("seckillNumber") long seckillNumber, // add_activity.html 中<input type="text" name="seckillNumber">
+            @RequestParam("startTime") String startTime,
+            @RequestParam("endTime") String endTime,
+            Map<String, Object> resultMap
+    ) throws ParseException {
+        startTime = startTime.substring(0, 10) + startTime.substring(11);
+        endTime = endTime.substring(0, 10) + endTime.substring(11);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-ddhh:mm");
+
+        SeckillActivity seckillActivity = new SeckillActivity();
+        seckillActivity.setName(name);
+        seckillActivity.setCommodityId(commodityId);
+        seckillActivity.setSeckillPrice(seckillPrice);
+        seckillActivity.setOldPrice(oldPrice);
+        seckillActivity.setTotalStock(seckillNumber);
+        seckillActivity.setAvailableStock(Integer.parseInt("" + seckillNumber)); // long 转 int
+        seckillActivity.setLockStock(0L);
+        seckillActivity.setActivityStatus(1);
+        seckillActivity.setStartTime(format.parse(startTime));
+        seckillActivity.setEndTime(format.parse(endTime));
+
+        // 插入数据库
+        seckillActivityDao.insertSeckillActivity(seckillActivity);
+
+        resultMap.put("seckillActivity", seckillActivity); // add_success.html 中用到 <span th:text="${seckillActivity.name}">
+
+        return "add_success";
+    }
+}
